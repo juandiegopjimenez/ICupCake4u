250658369397212657587e4e70fe2e5563359129
Juan Diego <juandpjimenez@gmail.com>
2020-08-29 02:28:15 -0500

    Clientes terminados

---
+++ php/correo.php
@@ -0,0 +1,10 @@
+<?php
+
+if (isset($_POST['enviar'])) {
+  if (!empty($_POST['name']) && !empty($_POST['asunto']) && !empty($_POST[0msg]) && !empty['email']) {
+    $name = $_POST['name'];
+    $asunto = $_POST['asunto']
+    $msg = $_POST['msg'];
+    $email
+  }
+}
